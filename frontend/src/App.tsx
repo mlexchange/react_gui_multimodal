8de541e90091cd513845c3de99986abb9db45b59
hpmartins <hpmartins@gmail.com>
2025-11-19 11:17:30 -0800

    commenting out hubapplayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,40 @@
 import { MantineProvider } from "@mantine/core";
-import { HubAppLayout } from "@blueskyproject/finch";
-import { RouteItem } from "@blueskyproject/finch";
+// import { HubAppLayout, RouteItem } from "@blueskyproject/finch";
 
 import { Scattering } from "./components/Scattering";
-import { XPS } from "./components/XPS";
+// import { XPS } from "./components/XPS";
 
-import { PlaceholderIcon } from "@phosphor-icons/react"
+// import { PlaceholderIcon } from "@phosphor-icons/react"
 
 import "@mantine/core/styles.css";
 import "@blueskyproject/finch/style.css";
 import "./index.css";
 
 function App() {
-  const routes: RouteItem[] = [
-    {
-      label: "Home",
-      path: "/",
-      element: (
-        <div>
-          <p>Home</p>
-        </div>
-      ),
-      icon: <PlaceholderIcon size={32} />,
-    },
-    {
-      label: "Scattering",
-      path: "/scattering",
-      element: <Scattering />,
-      icon: <PlaceholderIcon size={32} />,
-    },
-    {
-      label: "XPS",
-      path: "/xps",
-      element: <XPS />,
-      icon: <PlaceholderIcon size={32} />,
-    },
-  ];
+  // const routes: RouteItem[] = [
+  //   {
+  //     label: "Home",
+  //     path: "/",
+  //     element: (
+  //       <div>
+  //         <p>Home</p>
+  //       </div>
+  //     ),
+  //     icon: <PlaceholderIcon size={32} />,
+  //   },
+  //   {
+  //     label: "Scattering",
+  //     path: "/scattering",
+  //     element: <Scattering />,
+  //     icon: <PlaceholderIcon size={32} />,
+  //   },
+  //   {
+  //     label: "XPS",
+  //     path: "/xps",
+  //     element: <XPS />,
+  //     icon: <PlaceholderIcon size={32} />,
+  //   },
+  // ];
 
   return (
     <MantineProvider>
