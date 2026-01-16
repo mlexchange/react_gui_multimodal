2f73e9eff8ab1513b6cfcd1309b1c6498bcddb3b
hpmartins <hpmartins@gmail.com>
2026-01-16 00:13:49 -0800

    simplified App to use standalone only

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,11 @@
 import '@h5web/lib/styles.css';
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { ToastProvider } from "@/components/ui";
 import "./index.css";
 import App from "./app/App";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
-  </BrowserRouter>
+  <ToastProvider>
+    <App />
+  </ToastProvider>
 );
